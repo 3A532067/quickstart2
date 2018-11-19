1fdf8a61590f6613d837c82c9902d99faf3bede9
3A532067 <s3a532067@student.ncut.edu.tw>
2018-11-19 23:06:53 +0800

    TaskController取得tasks資料後，交由tasks/index.blade.php呈現出來

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,24 +3,29 @@
 namespace App\Http\Controllers;
 
 
-
+use App\Task;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
     //
+    protected $tasks;
 
-
-    public function __construct()
+    public function __construct(TaskRepository $tasks)
     {
         $this->middleware('auth');
+
+        $this->tasks = $tasks;
     }
 
     public function index(Request $request)
     {
-        return view('tasks.index');
+        return view('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
+        ]);
     }
 
 
