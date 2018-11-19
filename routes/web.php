441acc530e9d0cc7c23b7e9d26db97738cc445ef
3A532067 <s3a532067@student.ncut.edu.tw>
2018-11-19 11:53:54 +0800

    建立 TaskController & 連上 routes

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::get('/home', function () {
     return view('home.home');
 });
 
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
